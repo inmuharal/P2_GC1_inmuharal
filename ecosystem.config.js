2b19d5fc94d60e92fc6a30c4706dbb70c906569f
Inmuhar Algozy <inmuharalgozy@gmail.com>
2026-04-14 16:57:36 +0700

    release baru

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps : [{
+    name   : "63-server",
+    script : "bin/www",
+        env : {
+                NODE_ENV:"production",
+                PORT : 80,
+                DATABASE :"postgresql://postgres.cgoynyisujxotdknqxsz:ej1KfSPRkdMKxHn5@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
+                SECRET_KEY:'OKEGAS',
+                CLOUDINARY_CLOUD_NAME : "dmv5ni7ud",
+                CLOUDINARY_API_KEY : "988751142166531",
+                CLOUDINARY_API_SECRET : "NCUwfFGdjqcdTNXE6wyGwrAFMas"
+        }
+  }]
+}
