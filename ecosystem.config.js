a4f64ee803c835d287b2b467ec62e06e786e41e6
Inmuhar Algozy <inmuharalgozy@gmail.com>
2026-04-14 17:33:28 +0700

    fix:require

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         env : {
                 NODE_ENV:"production",
                 PORT : 80,
-                DATABASE :"postgresql://postgres.cgoynyisujxotdknqxsz:ej1KfSPRkdMKxHn5@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
+                DATABASE_URL :"postgresql://postgres.cgoynyisujxotdknqxsz:ej1KfSPRkdMKxHn5@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
                 SECRET_KEY:'OKEGAS',
                 CLOUDINARY_CLOUD_NAME : "dmv5ni7ud",
                 CLOUDINARY_API_KEY : "988751142166531",
